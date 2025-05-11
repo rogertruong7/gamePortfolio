af5f34c8328531a543ae8eb9b8dbbfdced5b537a
Roger <thejedipug@gmail.com>
2025-05-11 17:03:41 +1000

    made it so project info loads from .md for easier extension

---
+++ frontend/src/Pages/MarkdownSection.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+import {
+	Section,
+	Title,
+	Subtitle,
+	Paragraph,
+	List,
+	ListItem,
+	StyledTable,
+	StyledTh,
+	StyledTd,
+} from "./PageComponents.jsx";
+
+const MarkdownSection = ({ md }) => (
+	<Section>
+		<ReactMarkdown
+			remarkPlugins={[remarkGfm]}
+			components={{
+				h1: ({ node, ...props }) => <Title {...props} />,
+				h2: ({ node, ...props }) => <Subtitle {...props} />,
+				p: ({ node, ...props }) => <Paragraph {...props} />,
+				ul: ({ node, ordered, ...props }) => (
+				<List as={ordered ? "ol" : "ul"} {...props} />
+				),
+				li: ({ node, ...props }) => <ListItem {...props} />,
+				table: ({ node, ...props }) => <StyledTable {...props} />,
+				th: ({ node, ...props }) => <StyledTh {...props} />,
+				td: ({ node, ...props }) => <StyledTd {...props} />,
+			}}
+			>
+			{md}
+		</ReactMarkdown>
+	</Section>
+);
+
+export default MarkdownSection;

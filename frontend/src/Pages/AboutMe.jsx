aab0cd6d74cc44bff1a6b5f5a5098ac08bae95d8
Roger <thejedipug@gmail.com>
2025-05-11 23:41:29 +1000

    exit button" " git push

--- frontend/src/Pages/AboutMe.jsx
+++ frontend/src/Pages/AboutMe.jsx
@@ -31,6 +31,10 @@ const AboutMe = ({ setCurrentScene }) => {
         tw.stop();
         setCanClick(true);
         setSkipText(optionCount);
+
+        if (optionCount === 4) {
+          setEndOfText(true);
+        }
       }
     };
 
